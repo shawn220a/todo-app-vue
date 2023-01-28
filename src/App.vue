55f2b332951a1c6bb800bdcfb479c265acbff2dc
shawn220a <shawn.42496@gmail.com>
2023-01-27 21:23:34 -0600

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import List from './components/List.vue'
+</script>
+
+<template>
+  <h1>To Do App:</h1>
+  <!-- Todo List -->
+  <List />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/List.vue
@@ -0,0 +1,32 @@
+<script>
+let count = 0;
+export default {
+  data() {
+    return {
+      todoList: [],
+    };
+  },
+  methods: {
+    addTodo(t) {
+      this.todoList.push({ id: count, message: t });
+      count++;
+    },
+    removeTodo(item) {
+      let array = this.todoList.filter((x) => x.id !== item.id);
+      this.todoList = array;
+    },
+  },
+};
+</script>
+
+<template>
+  <!-- Todo List -->
+  <input v-model="text" />
+  <button @click="addTodo(text)">Add Todo</button>
+  <li v-for="todo in todoList">
+    {{ todo.message }}
+    <button @click="removeTodo(todo)">Delete</button>
+  </li>
+</template>
+
+<style scoped></style>
